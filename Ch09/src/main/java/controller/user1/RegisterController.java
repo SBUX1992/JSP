c741b52d7782c199842c6438db8de08421070b33
SBUX <sinsunkangg@naver.com>
2022-11-11 10:53:00 +0900

    2022-11-11 수업예제

---
+++ Ch09/src/main/java/controller/user1/RegisterController.java
@@ -0,0 +1,64 @@
+package controller.user1;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.User1Dao;
+import service.HelloService;
+import service.user1.ListService;
+import service.user1.ModifyService;
+import service.user1.RegisterService;
+import vo.User1Vo;
+
+@WebServlet("/user1/register.do")
+public class RegisterController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		requestProc(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		requestProc(req, resp);
+	}
+	
+	private void requestProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		if(req.getMethod().equals("GET")) {
+			// GET 요청
+			RegisterService service = RegisterService.getInstance();
+			String view = service.requestProc(req, resp);
+			
+			RequestDispatcher dispatcher = req.getRequestDispatcher(view);
+			dispatcher.forward(req, resp);
+			
+		}else {
+			// POST 요청
+			String uid  = req.getParameter("uid");
+			String name = req.getParameter("name");
+			String hp   = req.getParameter("hp");
+			String age  = req.getParameter("age");
+			
+			User1Vo vo = new User1Vo();
+			vo.setUid(uid);
+			vo.setName(name);
+			vo.setHp(hp);
+			vo.setAge(age);
+
+			User1Dao.getInstance().insertUser(vo);
+			
+			// 리다이렉트
+			resp.sendRedirect("/Ch09/user1/list.do");
+		}
+	}
+}
